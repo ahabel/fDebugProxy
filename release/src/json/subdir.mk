2df42a8cbcea2bc05947eea6614ce47d4bbd1ca6
Andreas Habel <virus-d@fcms.de>
2011-05-18 18:07:08 +0200

    path to /include changed

--- release/src/json/subdir.mk
+++ release/src/json/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 src/json/%.o: ../src/json/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -I"/storage/webspace/fdebug-proxy/include" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	gcc -I"../include" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
