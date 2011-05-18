9514a86448a01c702d896ca5f14e9b0af1b78820
Andreas Habel <virus-d@fcms.de>
2011-05-19 00:00:06 +0200

    temp test code for sqlite ClientDB

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -9,9 +9,21 @@
 #include "fdebugproxy.h"
 #include "json/cJSON.h"
 
+#include "db/database.h"
+
 using namespace std;
 
 int main(int argc, char **argv) {
+   fprintf(stdout, "starting...\n");
+
+   ClientDB *db = new ClientDB();
+   db->open("/tmp/x_fdebug.db");
+
+   //db->addClient("foo", "x", "1002");
+
+   sClient client = db->getClient("lol-foo");
+
+   return 0;
    fDebugProxy *proxy = new fDebugProxy();
    proxy->run();
    
