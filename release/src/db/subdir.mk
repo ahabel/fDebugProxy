e66d784429e883d5c48b9c4c2ac823a51dc02989
Andreas Habel <virus-d@fcms.de>
2011-05-19 10:05:18 +0200

    makefiles updated

---
+++ release/src/db/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/db/database.cpp 
+
+OBJS += \
+./src/db/database.o 
+
+CPP_DEPS += \
+./src/db/database.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/db/%.o: ../src/db/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -I"../include" -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o"$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
