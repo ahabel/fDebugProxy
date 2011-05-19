3c5814437026819442fdffd6f355981037e36933
Andreas Habel <virus-d@fcms.de>
2011-05-19 22:45:57 +0200

    Documentation added

--- include/db/database.h
+++ include/db/database.h
@@ -1,12 +1,6 @@
 /**
- *
- * SQLite Database Library
- *
- *
- * @author Andreas Habel
- * @package fDebugProxy
- * @class Database
- *
+ * @author     Andreas Habel
+ * @package    fDebugProxy
  *
  * Usage:
  *
@@ -28,24 +22,88 @@
 
 using namespace std;
 
+/**
+ * @struct  sClient
+ * @brief   Client property objects
+ */
 struct sClient {
-   string   uuid;
-   const char *   remote;
+   /**
+    * @property uuid
+    * Client UUID
+    */
+   string uuid;
+
+   /**
+    * @property remote
+    * Client Remote IP address
+    */
+   const char* remote;
+
+   /**
+    * @property port
+    * Client port
+    */
    int port;
 };
 
 
+/**
+ * @class   ClientDB
+ * @brief   Database Library
+ */
 class ClientDB {
 public:
    ClientDB();
+
+   /**
+    * Opens the connection to the sqlite database
+    * If client.db file does not exist, it will be created
+    *
+    * @param dbFile Path to SQLite database
+    */
    void     open(string dbFile);
+
+   /**
+    * Closes a opened database
+    */
    void     close();
+
+   /**
+    * Add a new fDebug Client to the database
+    *
+    * @param uuid    Client UUID
+    * @param remote  Remote IP address
+    * @param port    Remote Port
+    * @return true if the client was added to the DB or false in failure
+    */
    bool     addClient(string uuid, string remote, int port);
+
+   /**
+    * Returns the client settings (remote address and port) for a given UUID
+    *
+    * @param uuid    Client UUID
+    * @return new struct sClient with client settings
+    * @see sClient
+    */
    sClient  getClient(string uuid);
 
 private:
+
+   /**
+    * @property db
+    * Holds the sqlite3 database object
+    */
    sqlite3* db;
+
+   /**
+    * @property log
+    * Reference to the fSysLog instance
+    */
    fSysLog* log;
+
+   /**
+    * Created the internal tables and structures used for fdebug proxy
+    */
    void  createTables();
 };
 
