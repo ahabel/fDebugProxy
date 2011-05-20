a8be35bb8efd1fe48a5a8853d251e1f8cd97293e
Andreas Habel <virus-d@fcms.de>
2011-05-20 23:50:49 +0200

    use new json parser

--- include/fdebugproxy.h
+++ include/fdebugproxy.h
@@ -10,38 +10,28 @@
 #ifndef FDEBUGPROXY_H_
 #define FDEBUGPROXY_H_
 
-#include "json/cJSON.h"
 #include "fdebugsocket.h"
+#include "fdebugmessage.h"
 #include "db/database.h"
 
-
-//
-// fDebug Message
-//
-struct fDebugMessage {
-   char  *origin;
-   int   type;
-   cJSON *payload;
-};
-
+using namespace jsonxx;
 
 
 class fDebugProxy {
 public:
    fDebugProxy();
    void run();
-   void registerClient(fDebugMessage message);
-   void setClient(fDebugMessage message);
+   void registerClient(fDebugMessage *message);
+   void setClient(fDebugMessage *message);
    
 private:
    fSysLog        *log;
    fDebugSocket   *socket;
    ClientDB       *db;
    
-   void           handleControl(fDebugMessage message);
+   void           handleControl(fDebugMessage *message);
    bool           sendServer(const char* str);
-   bool           forwardData(fDebugMessage message);
-   fDebugMessage  parse(char *str);
+   bool           forwardData(fDebugMessage *message);
 };
 
 #endif /*FDEBUGPROXY_H_*/
