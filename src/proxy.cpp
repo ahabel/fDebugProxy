0b0e6176ba9fe7bffc6c491ab24607d2507531b2
Andreas Habel <virus-d@fcms.de>
2011-05-20 23:56:04 +0200

    code cleanup

--- src/proxy.cpp
+++ src/proxy.cpp
@@ -1,32 +1,7 @@
-#include <iostream>
-#include <getopt.h>
-#include "string.h"
-#include <stdlib.h>
-#include <stdio.h>
-
-#include "global.h"
-#include "syslog/fsyslog.h"
 #include "fdebugproxy.h"
-#include "json/cJSON.h"
-
-#include "db/database.h"
-
-using namespace std;
 
 int main(int argc, char **argv) {
-   /*fprintf(stdout, "starting...\n");
-
-   ClientDB *db = new ClientDB();
-   db->open("/tmp/x_fdebug.db");
-
-   //db->addClient("foo", "x", "1002");
-
-   sClient client = db->getClient("lol-foo");
-
-   return 0;*/
    fDebugProxy *proxy = new fDebugProxy();
    proxy->run();
-   
    return 0;
 }
-
