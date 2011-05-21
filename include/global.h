200b2e333a523391ac2ad33cb551d82e97dc47fa
Andreas Habel <virus-d@fcms.de>
2011-05-21 02:01:48 +0200

    removed unused definitions

--- include/global.h
+++ include/global.h
@@ -1,23 +1,13 @@
-
-
-#define NORMAL         "\033[0m"
-#define BOLD			  "\033[1m"
-#define COLOR_WARNING  "\033[0;33m" //yellow
-#define COLOR_ERROR    "\033[0;31m" //red
-#define COLOR_FATAL    "\033[1;31m" //red bold
-
-
 //
 // fDebug Config default settings
 //
-#define DEFAULT_PORT   		5005
 #define FDEBUG_LOGFILE     "/var/log/fdebug/proxy.log"
 #define FDEBUG_CLIENTDB    "/var/lib/fdebug/proxy.client.db"
 
 #define BUF_SIZE           10000
 
 // Enable Debug Mode
-// this option should not be used in produtional environments!
+// this option should not be used in production environments!
 #define DEBUG_MODE         false
 
 
